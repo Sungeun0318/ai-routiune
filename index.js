7756d7710dfe349521454f4c5a203bd57ee77693
Sungeun0318 <https://github.com/Sungeun0318/eatapp.git>
2025-05-08 17:52:38 +0900

    Co-authored-by: jaehong-maker <jaehong-maker@users.noreply.github.com>

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: 'http://localhost:3000',
+  credentials: true
 }));
 
 app.use(session({
